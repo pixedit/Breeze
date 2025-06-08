dd9bdafff38e344a5a9c0711c274669a5d8c456b
christmas <pixedit.2023@gmail.com>
2025-06-08 07:23:54 +0330

    fetch data and structure weatherContext

--- src/assets/api/api.js
+++ src/assets/api/api.js
@@ -1,1 +1,26 @@
 // const API = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
+
+import axios from "axios";
+
+const fetchCurrentWeather = async (latitude, longitude, dispatch) => {
+	try {
+		dispatch({ type: "SET_LOADING", payload: true });
+		const response = await axios.get("https://api.open-meteo.com/v1/forecast", {
+			params: {
+				latitude,
+				longitude,
+				current_weather: true,
+			},
+		});
+		dispatch({
+			type: "SET_CURRENT_WEATHER",
+			payload: response.data.current_weather,
+		});
+		dispatch({ type: "SET_LOADING", payload: false });
+	} catch (error) {
+		dispatch({ type: "SET_ERROR", payload: error.message });
+		dispatch({ type: "SET_LOADING", payload: false });
+	}
+};
+
+export default fetchCurrentWeather;
